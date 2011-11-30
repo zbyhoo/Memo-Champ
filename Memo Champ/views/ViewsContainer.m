c6c9f8e39b6c88b303fbba4886dff5dd93d8f9aa
zbyhoo <zbyhoo@gmail.com>
2011-12-01 00:07:13 +0100

    workaround for view transition after rotation

--- Memo Champ/views/ViewsContainer.m
+++ Memo Champ/views/ViewsContainer.m
@@ -12,6 +12,12 @@
 
 const NSTimeInterval TRANSITION_DURATION = 0.5f;
 
+@interface ViewsContainer (ViewsRotation)
+
+- (void) rotateNewView:(UIView*)newView toOrientationOfView:(UIView*)view;
+
+@end
+
 @implementation ViewsContainer
 
 + (ViewsContainer*) sharedContainer
@@ -53,11 +59,34 @@ const NSTimeInterval TRANSITION_DURATION = 0.5f;
 - (void) show:(ViewType)viewType fromView:(UIView*)view
 {
     UIView* viewToShow = ((UIViewController*)[viewControllers objectAtIndex:viewType]).view;
+    [self rotateNewView:viewToShow toOrientationOfView:view];
     
     [UIView transitionFromView:view toView:viewToShow 
                       duration:TRANSITION_DURATION 
                        options:UIViewAnimationOptionTransitionCrossDissolve
                     completion:nil];
+}
+
+#pragma mark - ViewsRotation
+
+- (void) rotateNewView:(UIView*)newView toOrientationOfView:(UIView*)view
+{
+    UIView* currentView = nil;
+    for (UIViewController* currentViewController in viewControllers)
+    {
+        if (currentViewController.view.superview)
+        {
+            currentView = currentViewController.view;
+            break;
+        }
+    }
+    [currentView removeFromSuperview];
+    
+    
+    [[[UIApplication sharedApplication] keyWindow] addSubview:newView];
+    [newView removeFromSuperview];
+    
+    [[[UIApplication sharedApplication] keyWindow] addSubview:currentView];
 }
 
 @end
